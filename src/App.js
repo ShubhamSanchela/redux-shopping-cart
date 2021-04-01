37387ba4c9f3b6ae746260df27923f85ffe64f4b
Shubham <srsshubh7@gmail.com>
2021-04-01 11:05:58 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Products from "./components/Products";
+import Cart from "./components/Cart";
+import About from "./components/About";
+import Details from "./components/Details";
+import Contact from "./components/Contact";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./components/Store";
+console.log(store)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Router>
+          {/* navbar */}
+          <Navbar />
+          {/* end of navbar */}
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/products" component={Products} />
+            <Route path="/cart" component={Cart} />
+            <Route path="/about" component={About} />
+            <Route path="/details/:id" component={Details} />
+            {/* <Route path="/contact" component={Contact} /> */}
+          </Switch>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
